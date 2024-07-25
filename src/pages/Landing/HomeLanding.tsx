88b7b3c70bece1173d9bc4c7755322049c7519e0
MomenMustafa45 <momenkara2@gmail.com>
2024-07-25 12:44:50 +0300

    feat: school status section added

--- src/pages/Landing/HomeLanding.tsx
+++ src/pages/Landing/HomeLanding.tsx
@@ -1,15 +1,47 @@
 import EnrollmentSlider from "../../components/EnrollmentSlider";
+import SchoolStatus from "../../components/SchoolStatus";
 import SliderMain from "../../components/SliderMain";
+import { LuTrees } from "react-icons/lu";
+import { GiTeacher } from "react-icons/gi";
+import { TbMathMaxMin } from "react-icons/tb";
+import { FaTree } from "react-icons/fa6";
+
+import StatusCard from "../../components/StatusCard";
 
 const HomeLanding = () => {
   return (
-    <main className="">
+    <main>
       <section>
         <SliderMain />
       </section>
-      <section className="px-10">
+      <section className="px-10 my-5">
         <EnrollmentSlider />
       </section>
+      <SchoolStatus>
+        <>
+          <StatusCard
+            title="Outdoor Activities"
+            number="75"
+            icon={<LuTrees size={30} color="white" />}
+          />
+          <StatusCard
+            title="Math Lessons"
+            number="237"
+            icon={<TbMathMaxMin size={30} color="white" />}
+          />
+          <StatusCard
+            title="Loving Teachers"
+            number="32"
+            icon={<GiTeacher size={30} color="white" />}
+          />
+          <StatusCard
+            title="Pencils Wood"
+            number="457"
+            icon={<FaTree size={30} color="white" />}
+          />
+        </>
+      </SchoolStatus>
+      {/* <h1 className="h-80">Hello</h1> */}
     </main>
   );
 };
