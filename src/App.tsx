7f607aa3a9026aa2917be4fddd019a5f017fef2e
MomenMustafa45 <momenkara2@gmail.com>
2024-08-07 16:15:40 +0300

    fix: routes and private routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
-import { Route, Routes } from "react-router-dom";
+// App.jsx
+import { Route, Routes, useLocation } from "react-router-dom";
+
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 import Schedule from "./pages/Dashboard/Schedule";
 import Grades from "./pages/Dashboard/Grades";
 import TeacherRoutine from "./pages/Dashboard/TeacherRoutine";
@@ -7,61 +10,74 @@ import AddParent from "./pages/Dashboard/AddParent";
 import AddStudent from "./pages/Dashboard/AddStudent";
 import AddTeacher from "./pages/Dashboard/AddTeacher";
 import HomeLanding from "./pages/Landing/HomeLanding";
-import "flowbite/dist/flowbite.css";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-import About from "../src/components/about/About.jsx";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-import Teachers from "./pages/Landing/Teachers.jsx";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-import Nav from "./components/Nav.jsx";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-import Contact from "./components/Contact.jsx";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-import Footer from "./components/about/Footer.jsx";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-import Login from "./components/Login.jsx";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-import NotFound from "./components/NotFund.jsx";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-import Grad from "./components/Grad.jsx";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-import Showgrad from "./components/Showgrad.jsx";
+import About from "./components/about/About";
+import Teachers from "./pages/Landing/Teachers";
+import Nav from "./components/Nav";
+import Contact from "./components/Contact";
+import Footer from "./components/about/Footer";
+import Login from "./components/Login";
+import NotFound from "./components/NotFund";
+import Grad from "./components/Grad";
+import Showgrad from "./components/Showgrad";
 import HomeDashboard from "./pages/Dashboard/HomeDashboard";
 
 function App() {
+  const location = useLocation();
+  const privateRoutes = [
+    "/dashboard",
+    "/grades",
+    "/schedule",
+    "/teacher-table",
+    "/student-table",
+    "/add-parent",
+    "/add-student",
+    "/add-teacher",
+  ];
+
+  const showNavAndFooter = !privateRoutes.includes(location.pathname);
+
   return (
     <>
-      {/* Header of the section */}
-      <Nav />
+      {showNavAndFooter && <Nav />}
       <Routes>
         <Route path="/" element={<HomeLanding />} />
         <Route path="/about" element={<About />} />
         <Route path="/stuff" element={<Teachers />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/grad" element={<Grad />} />
-        <Route path="/grad-two" element={<Showgrad />} />
-        <Route path="/dashboard" element={<HomeDashboard />} />
 
-        <Route path="/grades" element={<Grades />} />
-        <Route path="/contact" element={<Schedule />} />
-        <Route path="/teacher-table" element={<TeacherRoutine />} />
-        <Route path="/student-table" element={<StudentRoutine />} />
-        <Route path="/add-parent" element={<AddParent />} />
-        <Route path="/add-student" element={<AddStudent />} />
-        <Route path="/add-teacher" element={<AddTeacher />} />
+        {/* Protected routes */}
+        <Route
+          path="/dashboard"
+          element={<PrivateRoute element={HomeDashboard} />}
+        />
+        <Route path="/grades" element={<PrivateRoute element={Grades} />} />
+        <Route path="/schedule" element={<PrivateRoute element={Schedule} />} />
+        <Route
+          path="/teacher-table"
+          element={<PrivateRoute element={TeacherRoutine} />}
+        />
+        <Route
+          path="/student-table"
+          element={<PrivateRoute element={StudentRoutine} />}
+        />
+        <Route
+          path="/add-parent"
+          element={<PrivateRoute element={AddParent} />}
+        />
+        <Route
+          path="/add-student"
+          element={<PrivateRoute element={AddStudent} />}
+        />
+        <Route
+          path="/add-teacher"
+          element={<PrivateRoute element={AddTeacher} />}
+        />
+        <Route path="/grad" element={<PrivateRoute element={Grad} />} />
+        <Route path="/grad-two" element={<PrivateRoute element={Showgrad} />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-      <Footer />
+      {showNavAndFooter && <Footer />}
     </>
   );
 }
