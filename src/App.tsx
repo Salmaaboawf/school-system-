8ec96d10b69f621b93a90100ae3c8b002d8a7d0c
esraa-mohamed <esraamohamed1008@gmail.com>
2024-08-12 19:32:56 +0300

    my grades added

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import TeacherRoutine from "./pages/Dashboard/TeacherRoutine";
 import StudentRoutine from "./pages/Dashboard/StudentRoutine";
 import AddParent from "./pages/Dashboard/AddParent";
 import AddStudent from "./pages/Dashboard/AddStudent";
+// import MyGrades from "./pages/Dashboard/MyGrades";    // add to private route
 import AddTeacher from "./pages/Dashboard/AddTeacher";
 import HomeLanding from "./pages/Landing/HomeLanding";
 import About from "./components/about/About";
@@ -45,6 +46,9 @@ function App() {
         <Route path="/stuff" element={<Teachers />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
+
+        {/* <Route path="/teach" element={<TeacherRoutine />} /> */}
+
 
         {/* Protected routes */}
         <Route
