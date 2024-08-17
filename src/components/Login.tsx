0231dc0ab2ccd1809835ec472bd571d7bfe0fa6e
MomenMustafa45 <momenkara2@gmail.com>
2024-08-17 15:03:44 +0300

    fix: text changed

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -65,7 +65,7 @@ export default function Register() {
       </div>
 
       <h2 className="text-2xl font-bold mb-6 text-center text-blue-800">
-        School Registration
+        School Login
       </h2>
       <form
         onSubmit={handleSubmit(save, (err) => console.log(err))}
