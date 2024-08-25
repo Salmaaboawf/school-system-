80b7fce228a7cea8359ad1f12881a956e229034e
MomenMustafa45 <momenkara2@gmail.com>
2024-08-25 18:54:06 +0300

    login updated

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -10,12 +10,9 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import RaisingHandImage from "../assets/images/Raising hand-pana.png";
 import { useAuth } from "../hooks/useAuth";
-import { Select } from "flowbite-react";
 import { Radio } from "flowbite-react";
 
 export default function Login() {
-
-
   const userId = useAuth();
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
@@ -42,25 +39,30 @@ export default function Login() {
   });
 
   const save = async (value: { email: string; password: string }) => {
-  if(role){
-      const userCred = await  signInWithEmailAndPassword(auth, value.email, value.password)
-      
+    if (role) {
+      const userCred = await signInWithEmailAndPassword(
+        auth,
+        value.email,
+        value.password
+      );
+
       console.log(userCred.user.uid);
-      const isRightUser = await saveLoggedUser(userCred.user.uid, dispatch, role);
+      const isRightUser = await saveLoggedUser(
+        userCred.user.uid,
+        dispatch,
+        role
+      );
 
-      if(isRightUser){
-
+      if (isRightUser) {
         navigate("/", { replace: true });
-      } 
-    
-  }
+      }
+    }
   };
-
 
   function check(e: React.ChangeEvent<HTMLInputElement>) {
     console.log(e.target.value);
     setRole(e.target.value);
-}
+  }
   useEffect(() => {
     if (userId) {
       navigate("/about", { replace: true });
@@ -78,25 +80,33 @@ export default function Login() {
         School Login
       </h2>
       <div className="mb-6">
-        <fieldset
-          className="flex max-w-lg flex-row gap-10 items-center justify-center text-center mx-auto"
-        >
+        <fieldset className="flex max-w-lg flex-row gap-10 items-center justify-center text-center mx-auto">
           <legend className="mb-4">Choose your Role</legend>
           <div className="flex items-center gap-2">
-            <Radio id="teacher" name="countries" value="teachers"  onChange={check}/>
+            <Radio
+              id="teacher"
+              name="countries"
+              value="teachers"
+              onChange={check}
+            />
             <Label htmlFor="teacher">Teacher</Label>
           </div>
 
           <div className="flex items-center gap-2">
-            <Radio id="student" name="countries" value="students"  onChange={check}/>
+            <Radio
+              id="student"
+              name="countries"
+              value="students"
+              onChange={check}
+            />
             <Label htmlFor="student">Student</Label>
           </div>
           <div className="flex items-center gap-2">
-            <Radio id="" name="countries" value="parents"  onChange={check}/>
+            <Radio id="" name="countries" value="parents" onChange={check} />
             <Label htmlFor="">Parent</Label>
           </div>
           <div className="flex items-center gap-2">
-            <Radio id="" name="countries" value="users"  onChange={check}/>
+            <Radio id="" name="countries" value="users" onChange={check} />
             <Label htmlFor="">Admin</Label>
           </div>
         </fieldset>
