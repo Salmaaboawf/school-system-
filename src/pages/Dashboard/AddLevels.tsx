c4be90677d46a3588bbf63c40096efec6ab3b0fa
mostafa-madany <mostafamadny34@gmail.com>
2024-09-10 09:30:23 +0300

    bug fixed

--- src/pages/Dashboard/AddLevels.tsx
+++ src/pages/Dashboard/AddLevels.tsx
@@ -1,91 +1,11 @@
-// import { Button, Label, TextInput } from "flowbite-react";
-// import { addLevels } from "../../services/levelsServices";
-// import { useState } from "react";
-// import Header from "../../components/Header/Header";
-// import Sidebar from "../../components/Sidebar";
-// import * as Yup from "yup";
-
-// export default function AddLevels() {
-//   const [levelName, setLevelName] = useState();
-//   const [errors, setErrors] = useState({});
-
-//   const validationSchema = Yup.object().shape({
-//     levelName: Yup.string()
-//       .min(3, "min 3 letters")
-//       .matches(/^[A-Za-z\s]+$/,"must be just letters")
-//       .required("required "),
-//   });
-  
-
-//   const handleSave = async (event) => {
-//     event.preventDefault();
-
-//     try {
-//       await validationSchema.validate({ levelName }, { abortEarly: false });
-
-//       await addLevels(levelName);
-//       console.log("Level added to Firestore");
-//     } catch (error) {
-//       const validationErrors = {};
-//       error.inner.forEach((err) => {
-//         validationErrors[err.path] = err.message;
-//       });
-//       setErrors(validationErrors);
-//     }
-//   };
-
-//   return (
-//     <div className="container flex gap-x-5">
-//       <div className="flex-[1]">
-//         <Sidebar />
-//       </div>
-//       <div className="flex-[4]">
-//         {/* Header of the section */}
-//         <div>
-//           <Header />
-//         </div>
-//         {/* Header of the section */}
-//         <div className="my-5">
-//           <div className="flex justify-center items-center min-h-screen">
-//             <form className="w-72" onSubmit={handleSave}>
-//               <div className="mb-2 block">
-//                 <Label htmlFor="class" value="Class Name" />
-//               </div>
-//               <TextInput
-//                 id="class"
-//                 type="text"
-//                 placeholder="A1"
-              
-//                 value={levelName}
-//                 onChange={(e) => setLevelName(e.target.value)}
-//               />
-//               {errors.levelName && (
-//                 <div className="text-red-500">{errors.levelName}</div>
-//               )}
-//               <Button
-//                 outline
-//                 gradientDuoTone="pinkToOrange"
-//                 className="my-5 w-72"
-//                 type="submit"
-//               >
-//                 Add Class
-//               </Button>
-//             </form>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-//-----------------------
-import { useState } from "react";
 import { Button, Label, TextInput } from "flowbite-react";
 import { addLevels } from "../../services/levelsServices";
 import { useEffect, useState } from "react";
 import Header from "../../components/Header/Header";
-import Sidebar from "../../components/Sidebar";
 import * as Yup from "yup";
-import { toast} from 'react-toastify';
+import { toast } from "react-toastify";
+import Sidebar from "../../components/Sidebar";
+
 export default function AddLevels() {
   const [levelName, setLevelName] = useState("");
   const [errors, setErrors] = useState({});
@@ -93,11 +13,9 @@ export default function AddLevels() {
 
   const validationSchema = Yup.object().shape({
     levelName: Yup.string().required("Level is required"),
-      // .min(3, "min 3 letters")
-      // .matches(/^[A-Za-z\s]+$/,"must be just letters") 
-      
+    // .min(3, "min 3 letters")
+    // .matches(/^[A-Za-z\s]+$/,"must be just letters")
   });
-  
 
   const handleSave = async (event) => {
     event.preventDefault();
@@ -105,7 +23,7 @@ export default function AddLevels() {
     try {
       await validationSchema.validate({ levelName }, { abortEarly: false });
       await addLevels(levelName);
-      toast.success(`${levelName} grade added successfully`)
+      toast.success(`${levelName} grade added successfully`);
     } catch (error) {
       const validationErrors = {};
       error.inner.forEach((err) => {
@@ -116,15 +34,14 @@ export default function AddLevels() {
     }
   };
 
-  useEffect(()=>{
-    if(errors.levelName){
-      toast.error(errors.levelName)
+  useEffect(() => {
+    if (errors.levelName) {
+      toast.error(errors.levelName);
     }
-  },[errors])
+  }, [errors]);
 
   return (
     <div className="flex flex-col md:flex-row gap-5 p-5 mx-auto max-w-screen-lg">
-   
       <div
         className={`fixed inset-0 z-10 md:relative md:w-1/4 shadow-md transition-transform transform `}
       >
@@ -172,4 +89,3 @@ export default function AddLevels() {
     </div>
   );
 }
-
--- src/pages/Dashboard/AddSubject.tsx
+++ src/pages/Dashboard/AddSubject.tsx
@@ -1,214 +1,34 @@
-// import { useForm } from "react-hook-form";
-// import { yupResolver } from "@hookform/resolvers/yup";
-// import * as yup from "yup";
-// import Header from "../../components/Header/Header";
-// import Sidebar from "../../components/Sidebar";
-// import { Button, Select, Label, FileInput } from "flowbite-react";
-// import { useEffect, useState } from "react";
-// import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
-// import { fetchLevels } from "../../services/levelsServices";
-// import { addSubject } from "../../services/subjectServices";
-// import { uploadImageToStorage } from "../../services/subjectServices"; // استيراد الدالة
-// import { fetchTeachers } from "../../services/teacherServices";
-
-// // Validation Schema
-// const schema = yup.object().shape({
-//   name: yup
-//     .string()
-//     .matches(/^[A-Za-z\s]+$/, "must be characters only")
-//     .required("Required")
-//     .max(20, "Name cannot exceed 20 characters")
-//     .min(3, "Min is 3 letters"),
-
-//   teacher: yup.string().required("Required").min(3, "Min is 3 letters"),
-
-//   description: yup.string().required("Course description is required"),
-//   level_id: yup.string().required("Please select a class"),
-//   total_grade: yup
-//     .number()
-//     .required("Full mark is required")
-//     .positive("Grade must be a positive number"),
-//   photofile: yup.mixed().required("Photo is required"),
-// });
-
-// export default function AddSubject() {
-//   const {
-//     register,
-//     handleSubmit,
-//     formState: { errors },
-//     setValue,
-//   } = useForm({
-//     resolver: yupResolver(schema),
-//   });
-
-//   const levels = useAppSelector((state) => state.levels.levels);
-//   const dispatch = useAppDispatch();
-//   const [imagePreview, setImagePreview] = useState(null);
-//   const [teachers, setTeachers] = useState([]);
-
-//   // Save function to handle form submission
-//   const save = async (data) => {
-//     try {
-//       const photo = data.photofile;
-//       let photoURL = await uploadImageToStorage(photo);
-
-//       await addSubject({ ...data, photoURL });
-//       console.log("Subject added to Firestore");
-//     } catch (error) {
-//       console.error("Error adding subject: ", error);
-//     }
-//   };
-
-//   // Handle file input change
-//   const handlePhotoChange = (event) => {
-//     const file = event.target.files?.[0];
-//     if (file) {
-//       setImagePreview(URL.createObjectURL(file));
-//       setValue("photofile", file);
-//     }
-//   };
-
-//   // Fetch levels on component mount
-//   useEffect(() => {
-//     fetchLevels(dispatch);
-//     fetchTeachers().then((fetchedTeachers) => setTeachers(fetchedTeachers));
-//   }, [dispatch]);
-
-//   return (
-//     <div className="container flex gap-x-5">
-//       <div className="flex-[1]">
-//         <Sidebar />
-//       </div>
-//       <div className="flex-[4]">
-//         <Header />
-//         <div className="my-5">
-//           <section className="shadow-md text-[#002749]">
-//             <h3 className="bg-[#002749] text-white font-bold py-4 pl-4 text-lg">
-//               Add Subject
-//             </h3>
-//             <form onSubmit={handleSubmit(save)} className="p-4 w-full">
-//               <div className="mb-4">
-//                 <label htmlFor="courseName">Course Name</label>
-//                 <input
-//                   type="text"
-//                   className="block border pl-2 w-full mt-2 py-1 border-gray-300 rounded"
-//                   id="courseName"
-//                   placeholder="Course Name"
-//                   {...register("name")}
-//                 />
-//                 <p className="text-red-500">{errors.name?.message}</p>
-//               </div>
-//               <div className="mb-4">
-//                 <label htmlFor="courseMark">Course Full Mark</label>
-//                 <input
-//                   type="number"
-//                   className="block border pl-2 w-full mt-2 py-1 border-gray-300 rounded"
-//                   id="courseMark"
-//                   placeholder="Course Full Mark"
-//                   {...register("total_grade")}
-//                 />
-//                 <p className="text-red-500">{errors.total_grade?.message}</p>
-//               </div>
-
-//               <div>
-//                 <Label htmlFor="class" value="Class" />
-
-//                 <Select id="class" {...register("level_id")}>
-//                   <option value="">Select</option>
-//                   {levels.map((lvl) => (
-//                     <option key={lvl.id} value={lvl.id}>
-//                       {lvl.name}
-//                     </option>
-//                   ))}
-//                 </Select>
-//                 <p className="text-red-500">{errors.level_id?.message}</p>
-//               </div>
-
-//               {/* Teacher Dropdown */}
-//               <div className="mb-4">
-//                 <Label htmlFor="teacher" value="Select Teacher" />
-//                 <Select id="teacher" {...register("teacher")}>
-//                   <option value="">Select a Teacher</option>
-//                   {teachers.map((teacher) => (
-//                     <option key={teacher.id} value={teacher.id}>
-//                       {teacher.name}
-//                     </option>
-//                   ))}
-//                 </Select>
-//                 <p className="text-red-500">{errors.teacher?.message}</p>
-//               </div>
-
-//               <div className="mb-4">
-//                 <label htmlFor="description">Course Description</label>
-//                 <textarea
-//                   id="description"
-//                   rows={4}
-//                   placeholder="Enter course description here..."
-//                   className="block border pl-2 w-full mt-2 py-1 border-gray-300 rounded"
-//                   {...register("description")}
-//                 />
-//                 <p className="text-red-500">{errors.description?.message}</p>
-//               </div>
-
-//               {/* Photo field */}
-//               <div>
-//                 <Label htmlFor="photo" value="Teacher Photo" />
-//                 <FileInput
-//                   id="photo"
-//                   accept="image/*"
-//                   onChange={handlePhotoChange}
-//                 />
-//                 <p className="text-red-500">{errors.photofile?.message}</p>
-//                 {imagePreview && (
-//                   <img src={imagePreview} alt="Preview" className="my-4" />
-//                 )}
-//               </div>
-
-//               <div className="">
-//                 <Button
-//                   outline
-//                   gradientDuoTone="pinkToOrange"
-//                   className="my-5 w-72"
-//                   type="submit"
-//                 >
-//                   Add
-//                 </Button>
-//               </div>
-//             </form>
-//           </section>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-//---------------
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import Header from "../../components/Header/Header";
 import Sidebar from "../../components/Sidebar";
 import { Button, Select, Label, FileInput } from "flowbite-react";
-import { addQuestion, addSubject, uploadImageToStorage } from "../../services/subjectServices";
-import { useEffect, useState } from "react";
-import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
-import { fetchLevels } from "../../services/levelsServices";
-import { toast } from 'react-toastify';
 import {
+  addQuestion,
   addSubject,
   uploadImageToStorage,
 } from "../../services/subjectServices";
+import { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
+import { fetchLevels } from "../../services/levelsServices";
+import { toast } from "react-toastify";
+
 import { fetchTeachers } from "../../services/teacherServices";
 const schema = yup.object().shape({
   name: yup
     .string()
     .matches(/^[A-Za-z\s]+$/, "Subject name must be characters only")
     .required("required ")
-    .max(20, "Subject name cannot exceed 20 characters").min(3, "min is 3 letters"),
+    .max(20, "Subject name cannot exceed 20 characters")
+    .min(3, "min is 3 letters"),
 
-  teacher: yup.string()
+  teacher: yup
+    .string()
     .matches(/^[A-Za-z\s]+$/, "Teacher name must be characters only")
     .required("required ")
-    .max(20, " name cannot exceed 20 characters").min(3, "Teacher name must be at least 3 letters"),
+    .max(20, " name cannot exceed 20 characters")
+    .min(3, "Teacher name must be at least 3 letters"),
   description: yup.string().required("Course description is required"),
   level_id: yup.string().required("Please select a class"),
   total_grade: yup
@@ -275,10 +95,11 @@ export default function AddSubject() {
     }
   }, [errors]);
 
-
   return (
     <div className="flex flex-col md:flex-row gap-5 p-5 mx-auto max-w-screen-lg">
-      <div className={`fixed inset-0 z-10 md:relative md:w-1/4 shadow-md transition-transform transform `}>
+      <div
+        className={`fixed inset-0 z-10 md:relative md:w-1/4 shadow-md transition-transform transform `}
+      >
         <Sidebar />
       </div>
 
