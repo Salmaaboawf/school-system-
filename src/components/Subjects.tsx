029097293a1fc98a8c715ac4c6e99b992aff6dbb
esraa-mohamed <esraamohamed1008@gmail.com>
2024-09-10 10:07:31 +0300

    subject cards

--- src/components/Subjects.tsx
+++ src/components/Subjects.tsx
@@ -5,7 +5,8 @@ import { fetchSubjectsByLevel } from '../services/subjectServices';
 import { useAppSelector } from '../hooks/reduxHooks';
 import { useNavigate } from 'react-router-dom';
 import Nav from './Nav';
-
+import '../assets/style.css';
+import { MdCalculate } from 'react-icons/md';
 function Subjects() {
   const [filteredSubjects, setFilteredSubjects] = useState([]);
   const dispatch = useDispatch();
@@ -19,37 +20,59 @@ function Subjects() {
         setFilteredSubjects([...data]);
       }
     };
-  
+
     loadSubjects();
   }, [dispatch, userInfo.class_id]);
 
   const handleButtonClick = () => {
-    navigate('/quiz'); 
+    navigate('/quiz');
   };
 
   return (
     <>
-    <Nav/>
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
-      {filteredSubjects.map((subject) => (
-        <Card
-          key={subject.id}
-          className="max-w-sm"
-          imgAlt={`صورة خاصة بمادة ${subject.name}`}
-          imgSrc={subject.photoURL || "/default-image.jpg"}
-        >
-          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {subject.name}
-          </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            {subject.description}
-          </p>
-          <Button onClick={handleButtonClick} className="mt-4">
-            Take Exam
-          </Button>
-        </Card>
-      ))}
-    </div>
+      <Nav />
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
+        {filteredSubjects.map((subject) => (
+
+          <section className="card-section">
+            <div className="card">
+              <div className="flip-card">
+                <div className="flip-card__container">
+                  <div className="card-front">
+                    <div className="card-front__tp card-front__tp--city overflow-hidden">
+                     
+                      <div className='w-full h-full overflow-hidden'>
+                        <img src={subject.photoURL} className='w-full h-full' />
+                      </div>
+                      {/* <h2 className="card-front__heading">
+                        {subject.name}
+                      </h2> */}
+                    </div>
+
+                    <div className="card-front__bt">
+                      <p className="card-front__text-view card-front__text-view--city">
+                        View me
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="inside-page">
+                <div className="inside-page__container">
+                  <h3 className="inside-page__heading inside-page__heading--city">
+                    {subject.name}
+                  </h3>
+                  <p className="inside-page__text">
+                    {subject.description}
+                  </p>
+                  <a href="#" className="inside-page__btn inside-page__btn--city" onClick={handleButtonClick}>See Materials</a>
+                </div>
+              </div>
+            </div>
+          </section>
+        ))}
+      </div>
     </>
   );
 }
