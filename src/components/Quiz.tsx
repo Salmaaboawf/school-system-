4732d5be0cf8de919c9b231b2818f90daf78eff8
MomenMustafa45 <momenkara2@gmail.com>
2024-10-03 11:37:01 +0300

    edit users added

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,190 +1,3 @@
-// import { useEffect, useState } from "react";
-// import img from "../assets/images/Blue Playful Space Landscape Blank Page Border.png";
-// import { useAppSelector } from "../hooks/reduxHooks";
-// import {
-//   addGradeToStudent,
-//   checkIfStudentCompletedQuiz,
-//   getQuizQuestions,
-//   markQuizAsCompleted,
-// } from "../services/quizServices";
-// import { useLocation, useNavigate } from "react-router-dom";
-// import Swal from "sweetalert2";
-
-// const QuizPage = () => {
-//   const divStyle = {
-//     backgroundImage: `url(${img})`,
-//     backgroundSize: "contain",
-//     backgroundRepeat: "no-repeat",
-//     backgroundPosition: "center",
-//     minHeight: "100vh",
-//     display: "flex",
-//     justifyContent: "center",
-//     alignItems: "center",
-//     padding: "20px",
-//   };
-
-//   const userInfo = useAppSelector((state) => state.user.user);
-//   const [score, setScore] = useState(0);
-//   const [quizCompleted, setQuizCompleted] = useState(false);
-//   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-//   const [quizQuestions, setQuizQuestions] = useState([]);
-//   const [answer, setAnswer] = useState("");
-//   const [subject, setSubject] = useState(null); // Initialize with null
-//   const navigate = useNavigate();
-//   const location = useLocation();
-//   const { subjectId } = location.state || {};
-
-//   // Ensure subjectId is properly assigned and is not undefined
-//   useEffect(() => {
-//     if (subjectId) {
-//       setSubject(subjectId); // Set subjectId once it's available
-//     }
-//   }, [subjectId]);
-
-//   // Check if student has already taken the quiz
-//   const isVisitedQuizBefore = async (subject: string) => {
-//     try {
-//       const isVisited = await checkIfStudentCompletedQuiz(subject, userInfo.id);
-//       if (isVisited) {
-//         Swal.fire({
-//           icon: "error",
-//           title: "Oops...",
-//           text: "You can't take the same quiz two times",
-//         }).then((res) => {
-//           if (res.isConfirmed) {
-//             navigate("/");
-//           }
-//         });
-//       }
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-
-//   // Fetch quiz questions and handle quiz state
-//   useEffect(() => {
-//     if (subject) {
-//       getQuizQuestions(subject, (questions) => {
-//         setQuizQuestions([...questions]);
-//         if (questions.length < 20) {
-//           Swal.fire({
-//             icon: "error",
-//             title: "Oops...",
-//             text: "Exam not ready yet.",
-//           }).then((res) => {
-//             if (res.isConfirmed) {
-//               navigate("/");
-//             }
-//           });
-//         }
-//       });
-
-//       isVisitedQuizBefore(subject);
-//     }
-//   }, [subject]);
-
-//   const currentQuestion = quizQuestions[currentQuestionIndex];
-
-//   // Handle next question
-//   const handleNextQuestion = () => {
-//     if (answer) {
-//       if (currentQuestionIndex < quizQuestions.length) {
-//         if (
-//           currentQuestion.options.indexOf(answer) ===
-//           currentQuestion.correctAnswer
-//         ) {
-//           setScore((prev) => prev + 1);
-//         }
-
-//         if (currentQuestionIndex === quizQuestions.length - 1) {
-//           setQuizCompleted(true);
-//           markQuizAsCompleted(subject, userInfo.id);
-//           addGradeToStudent({
-//             studentId: userInfo.id,
-//             subjectId: subject,
-//             level_id: userInfo.class_id,
-//             score: (score + 1).toString(),
-//           });
-//         } else {
-//           setCurrentQuestionIndex((prev) => prev + 1);
-//         }
-//         setAnswer("");
-//       }
-//     }
-//   };
-
-//   const handleOptionChange = (e) => {
-//     setAnswer(e.target.value);
-//   };
-
-//   return (
-//     <>
-//       <div style={divStyle}>
-//         <div className="bg-white p-8 rounded-lg  max-w-lg w-full">
-//           {!quizCompleted ? (
-//             <>
-//               <h1 className="text-2xl font-bold mb-4 text-center">{`Quiz: ${currentQuestion?.question}`}</h1>
-
-//               <div className="mb-4">
-//                 {currentQuestion?.options.map((option, index) => (
-//                   <div key={index} className="flex items-center mb-2">
-//                     <input
-//                       type="radio"
-//                       id={`option${index}`}
-//                       name="quizOption"
-//                       value={option}
-//                       onChange={handleOptionChange}
-//                       className="mr-2"
-//                     />
-//                     <label htmlFor={`option${index}`} className="text-lg">
-//                       {option}
-//                     </label>
-//                   </div>
-//                 ))}
-//               </div>
-
-//               <div className="flex justify-between mt-4">
-//                 <button
-//                   onClick={handleNextQuestion}
-//                   className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
-//                 >
-//                   Next
-//                 </button>
-//               </div>
-//             </>
-//           ) : (
-//             <div>
-//               <h1 className="text-2xl font-bold mb-4 text-center">
-//                 Quiz Completed!
-//               </h1>
-//               <h1 className="text-2xl font-bold mb-4 text-center">
-//                 Your Score is {score} from {quizQuestions.length}
-//               </h1>
-//               <p className="text-lg text-center">
-//                 Thank you for completing the quiz. Your answers have been
-//                 recorded.
-//               </p>
-//               <div className="flex justify-center mt-4">
-//                 <button
-//                   onClick={() => {
-//                     navigate("/");
-//                   }}
-//                   className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
-//                 >
-//                   Go Home
-//                 </button>
-//               </div>
-//             </div>
-//           )}
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default QuizPage;
-//-----------------
-
 import { useEffect, useState, useRef } from "react";
 import img from "../assets/images/Blue Playful Space Landscape Blank Page Border.png";
 import { useAppSelector } from "../hooks/reduxHooks";
@@ -196,7 +9,7 @@ import {
 } from "../services/quizServices";
 import { useLocation, useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
-import '../assets/general.css'
+import "../assets/general.css";
 const QuizPage = () => {
   // const divStyle = {
   //   backgroundImage: `url(${img})`,
@@ -221,17 +34,12 @@ const QuizPage = () => {
   const location = useLocation();
   const { subjectId } = location.state || {};
 
-  // Ref for the video element
-  const videoRef = useRef(null);
-
   // Ensure subjectId is properly assigned and is not undefined
   useEffect(() => {
     if (subjectId) {
       setSubject(subjectId); // Set subjectId once it's available
     }
   }, [subjectId]);
-
-  console.log(quizQuestions, currentQuestionIndex);
 
   // Check if student has already taken the quiz
   const isVisitedQuizBefore = async (subject: string) => {
@@ -256,10 +64,13 @@ const QuizPage = () => {
   // Fetch quiz questions and handle quiz state
   useEffect(() => {
     if (subject) {
+      isVisitedQuizBefore(subject);
+
       // Fetch quiz questions
       getQuizQuestions(subject, (questions) => {
         // Check if student's level matches
-        if (userInfo.class_id !== level_id) {
+
+        if (!subject) {
           Swal.fire({
             icon: "error",
             title: "Access Denied",
@@ -271,11 +82,11 @@ const QuizPage = () => {
           });
           return; // Exit if levels don't match
         }
-  
+
         // Proceed if level matches
         setQuizQuestions([...questions]);
-  
-        if (questions.length < 20) {
+
+        if (questions.length < 4) {
           Swal.fire({
             icon: "error",
             title: "Oops...",
@@ -287,11 +98,8 @@ const QuizPage = () => {
           });
         }
       });
-  
-      isVisitedQuizBefore(subject);
     }
   }, [subject, userInfo.class_id]);
-  
 
   const currentQuestion = quizQuestions[currentQuestionIndex];
 
@@ -300,11 +108,13 @@ const QuizPage = () => {
     if (answer) {
       if (currentQuestionIndex < quizQuestions.length) {
         if (
-          currentQuestion.options.indexOf(answer) ===
+          currentQuestion.options.indexOf(answer) ==
           currentQuestion.correctAnswer
         ) {
           setScore((prev) => prev + 1);
+          console.log("correct ", score);
         }
+        console.log("sss", score);
 
         if (currentQuestionIndex === quizQuestions.length - 1) {
           setQuizCompleted(true);
@@ -313,7 +123,7 @@ const QuizPage = () => {
             studentId: userInfo.id,
             subjectId: subject,
             level_id: userInfo.class_id,
-            score: (score + 1).toString(),
+            score: score.toString(),
           });
         } else {
           setCurrentQuestionIndex((prev) => prev + 1);
@@ -322,7 +132,6 @@ const QuizPage = () => {
       }
     }
   };
-
 
   const handleOptionChange = (e) => {
     setAnswer(e.target.value);
@@ -356,10 +165,8 @@ const QuizPage = () => {
                 ))}
               </div> */}
                 <div className="mb-4">
-
-
                   {currentQuestion?.options.map((option, index) => {
-                    const letters = ['A', 'B', 'C', 'D']; // Add more if needed based on options length
+                    const letters = ["A", "B", "C", "D"]; // Add more if needed based on options length
                     return (
                       <div
                         key={index}
@@ -374,7 +181,7 @@ const QuizPage = () => {
                             value={option}
                             onChange={handleOptionChange}
                             className="absolute opacity-0 peer"
-                          // checked={answer === option}
+                            // checked={answer === option}
                           />
                           {/* Custom radio button */}
                           <div className="w-8 h-8 border-2 border-gray-700 rounded-full flex items-center justify-center cursor-pointer peer-checked:bg-orange-500 peer-checked:border-orange-500">
@@ -389,7 +196,6 @@ const QuizPage = () => {
                       </div>
                     );
                   })}
-
                 </div>
                 <div className="flex justify-between mt-4">
                   <button
